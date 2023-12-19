3b514757a0c749854cc29a8c60dda8010599ee2a
github isobe-00 <isobe-k@smhc.co.jp>
2023-12-19 11:00:28 +0900

    12/19 画面遷移図

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,11 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    /**
+     * モデルで代入可能な属性。
+     *
+     * @var array
+     */
     protected $fillable = [
-        'category_id',
-        'amount',
-        'type',
-        'transaction_date',
-        'memo',
+        'category_id',         // カテゴリID
+        'amount',              // 金額
+        'type',                // 収支種別 (income: 収入, expense: 支出)
+        'transaction_date',    // 取引日付
+        'memo',                // メモ
     ];
 }
